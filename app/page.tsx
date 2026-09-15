d29fdb07fd0597eb9d62ddfef6ab6b4c5f0559f6
20tksh21 <20tksh21@gmail.com>
2026-09-15 10:33:44 +0900

    イベント追加機能実装
    イベント名（必須）、
    必要フラグ、
    獲得フラグ、
    説明
    の4つを文字列で入力
    IDはどうするか未定なので、とりあえず現在の時刻にした

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,59 @@
-import { GameEvents } from "@/data/mockEvents";
+'use client';
+
+import { useState } from "react";
+import { GameEvents as InitialEvents } from "@/data/mockEvents";
+import type { GameEvent } from "@/types/gameEvent";
 
 export default function MainPage() {
+
+    const [gameEvents, setGameEvent] = useState<GameEvent[]>(InitialEvents)
+
+    const [title, setTitle] = useState<string>("");
+    const [needFlag, setNeedFlag] = useState<string>("");
+    const [getFlag, setGetFlag] = useState<string>("");
+    const [description, setDescription] = useState<string>("");
+
+    const handleAddEvent = () => {
+        if(!title) return;
+
+        const needFlagArray = needFlag ? needFlag.split(",").map(n => n.trim()) : [];
+
+        const newGameEvent: GameEvent = {
+            id: Date.now(),     //仮設定で現在時刻
+            title: title,
+            needFlag: needFlagArray,
+            getFlag: getFlag,
+            description: description
+        };
+
+        setGameEvent([...gameEvents, newGameEvent]);
+
+        setTitle("");
+        setNeedFlag("");
+        setGetFlag("");
+        setDescription("");
+    }
+
     return (
         <main>
-            <h2>イベント一覧</h2>
-            <hr/>
-            {GameEvents.map((gameEvent) => (
-                <div key={gameEvent.id}>
-                    <p>{gameEvent.title}</p>
-                    <p>必要フラグ：{gameEvent.needFlag?.join(", ") || "なし"}</p>
-                    <p>獲得フラグ：{gameEvent.getFlag || "なし"}</p>
-                    {gameEvent.description && <p>{gameEvent.description}</p>}
-                    <hr/>
+            <div>
+                <h2>イベント入力</h2>
+                <input type="text" placeholder="イベント名(必須)" onChange={(e) => setTitle(e.target.value)} value={title}></input>
+                <input type="text" placeholder="必要フラグ名" onChange={(e) => setNeedFlag(e.target.value)} value={needFlag}></input>
+                <input type="text" placeholder="獲得フラグ名" onChange={(e) => setGetFlag(e.target.value)} value={getFlag}></input>
+                <input type="text" placeholder="説明" onChange={(e) => setDescription(e.target.value)} value={description}></input>
+                <button onClick={handleAddEvent}>追加</button>
+            </div>
+
+            <hr></hr>
+
+            {gameEvents.map((gEvent) => (
+                <div key={gEvent.id}>
+                    <p>{gEvent.title}</p>
+                    <p>必要フラグ：{gEvent.needFlag?.join(", ") || "なし"}</p>
+                    <p>獲得フラグ：{gEvent.getFlag || "なし"}</p>
+                    {gEvent.description && <p>{gEvent.description}</p>}
+                    <hr></hr>
                 </div>
             ))}
         </main>
